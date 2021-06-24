3b81fa5e173fb9d0b8bba2cf67febd99047db7fc
ossington <33334871+ossington@users.noreply.github.com>
2021-06-24 08:39:03 +0100

    Add files via upload

--- ssmItems.js
+++ ssmItems.js
@@ -39,16 +39,16 @@ barText="SIDE MENU"; // <IMG> tag supported. Put exact html for an image to show
 // ssmItems[...]=[name, link, target, colspan, endrow?] - leave 'link' and 'target' blank to make a header
 ssmItems[0]=["Menu"] //create header
 ssmItems[1]=["Home", "index.html", ""]
-ssmItems[2]=["Marriages 1607-1837", "page8.html",""]
-ssmItems[3]=["Census 1841", "page6.html", ""]
-ssmItems[4]=["Census 1851", "page2.html", ""]
-ssmItems[5]=["Census 1861", "page13.html", ""]
-ssmItems[6]=["Census 1871", "page30.html", ""]
-ssmItems[7]=["Census 1881", "page4.html", ""]
-ssmItems[8]=["Census 1891", "page11.html", ""]
-ssmItems[9]=["Census 1901", "page20.html", ""]
-ssmItems[10]=["Census 1911", "page40.html", ""]
-ssmItems[11]=["", "", ""]
+ssmItems[2]=["Marriages 1607-1837", "1607.html",""]
+ssmItems[3]=["Census 1841", "1841.html", ""]
+ssmItems[4]=["Census 1851", "1851.html", ""]
+ssmItems[5]=["Census 1861", "1861.html", ""]
+ssmItems[6]=["Census 1871", "1871.html", ""]
+ssmItems[7]=["Census 1881", "1881.html", ""]
+ssmItems[8]=["Census 1891", "1891.html", ""]
+ssmItems[9]=["Census 1901", "1901.html", ""]
+ssmItems[10]=["Census 1911", "1911.html", ""]
+ssmItems[11]=["", ".html", ""]
 ssmItems[12]=["Laxton Folks", "page51.html", ""]
 
 buildMenu();
